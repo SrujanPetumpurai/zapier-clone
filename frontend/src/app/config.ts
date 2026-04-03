140045aeff781bfef142e6b830270d2e02db4113
P.Srujan <srujan.petumpurai@gmail.com>
2026-04-03 14:45:28 +0530

    ready for deployment

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -1,8 +1,8 @@
 
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000"
 export const HOOKS_URL = "http://localhost:3002"
 export const PROVIDER_CONFIG: Record<string, { connectUrl: string }> = {
-  google: { connectUrl: `${BACKEND_URL}api/v1/google` },
-  github: { connectUrl: `${BACKEND_URL}api/v1/github` },
+  google: { connectUrl: `${BACKEND_URL}/api/v1/google` },
+  github: { connectUrl: `${BACKEND_URL}/api/v1/github` },
   gmail:{connectUrl:`${BACKEND_URL}/api/v1/gmail/auth`}
 }
